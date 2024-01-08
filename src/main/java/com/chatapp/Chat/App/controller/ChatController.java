5a534e44df5e1cdaa6f83d47c50f5025b3d45611
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-08 02:11:34 +0000

    Update

--- src/main/java/com/chatapp/Chat/App/controller/ChatController.java
+++ src/main/java/com/chatapp/Chat/App/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.chatapp.Chat.App.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,6 +13,13 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
 
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 
